f0efe51a896ac640d9b7b50e6d216d925287f2cc
Noel Lim <noellimx@gmail.com>
2022-08-20 02:39:21 +0800

    submission 3 final

--- recodefy/3/3.R
+++ recodefy/3/3.R
@@ -5,30 +5,30 @@ print(nrow(iris)) # nrow is row
 
 
 # head rows
-count_head = 10
-sample_head = head(iris, count_head)
+count_head <- 10
+sample_head <- head(iris, count_head)
 stopifnot(nrow(sample_head) == count_head)
 
 # column head values
-first_three_values_of_sepal_length = iris$Sepal.Length[1:3]
+first_three_values_of_sepal_length <- iris$Sepal.Length[1:3]
 stopifnot(length(first_three_values_of_sepal_length) == 3)
 
 # column access
 
 stopifnot(iris$Sepal.Length == iris[["Sepal.Length"]])
 
-stopifnot(iris$Sepal.Length == iris[,1])
+stopifnot(iris$Sepal.Length == iris[, 1])
 # row access
 
-count_head = 1
-sample_head = head(iris, count_head)
+count_head <- 1
+sample_head <- head(iris, count_head)
 
-sample_head_by_brackets = iris[count_head,]
+sample_head_by_brackets <- iris[count_head, ]
 stopifnot(sample_head == sample_head_by_brackets)
 
 # grid access
 
-sub_grid= iris[1:2,1:3]
+sub_grid <- iris[1:2, 1:3]
 stopifnot(nrow(sub_grid) == 2)
 stopifnot(length(sub_grid) == 3)
 
@@ -42,28 +42,29 @@ stopifnot(ncol(iris_2) == 4)
 
 iris_2$SomeNewCol <- iris_2$Petal.Width * 2
 
-stopifnot(ncol(iris_2) == 5 )
+stopifnot(ncol(iris_2) == 5)
 
 
 SomeNewOrphanCol <- iris_2$Sepal.Width * 3
 
-attached_col = cbind(iris_2,SomeNewOrphanCol)
+attached_col <- cbind(iris_2, SomeNewOrphanCol)
 print(ncol(attached_col))
 stopifnot(ncol(attached_col) == 6)
 
-    
-attached_row = rbind(iris_2,c(1,2,3,"setosa",2))
+
+attached_row <- rbind(iris_2, c(1, 2, 3, "setosa", 2))
 
 stopifnot(nrow(attached_row) - 1 == nrow(iris_2))
 
-print(colnames(iris_2)
-      )
+print(colnames(iris_2))
 
-newcolnames = c("1",
-                "2",
-                "3",
-                "4",
-                "5")
+newcolnames <- c(
+    "1",
+    "2",
+    "3",
+    "4",
+    "5"
+)
 
 colnames(iris_2) <- newcolnames
 stopifnot(colnames((iris_2))[1] == "1")
@@ -71,16 +72,16 @@ stopifnot(colnames((iris_2))[1] == "1")
 
 # filter by column values
 
-setosas = iris[iris$Species == "setosa",]
+setosas <- iris[iris$Species == "setosa", ]
 print(nrow(setosas))
 
 
 # Dataframe Generation
 
-col1 <- c("col1val1","col1val2")
-col2 <- c("col2val2","col2val2")
+col1 <- c("col1val1", "col1val2")
+col2 <- c("col2val2", "col2val2")
 
-mydata = data.frame(col1,col2)
+mydata <- data.frame(col1, col2)
 stopifnot(colnames(mydata)[1] == "col1")
 stopifnot(colnames(mydata)[2] == "col2")
 
@@ -89,9 +90,9 @@ url_csv <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/css
 
 csv_data <- read.csv(url_csv)
 
-wanted_columns = c("Country_Region", "Confirmed", "Deaths")
+wanted_columns <- c("Country_Region", "Confirmed", "Deaths")
 
-csv_data_with_wanted_columns = csv_data[1:10,wanted_columns]
+csv_data_with_wanted_columns <- csv_data[1:10, wanted_columns]
 
 setwd("~/")
 
@@ -102,5 +103,5 @@ write.table(csv_data_with_wanted_columns, "dumb.table") # space delimited
 
 
 # value count
-column_is_a_vector = iris[,1]
+column_is_a_vector <- iris[, 1]
 stopifnot(length(column_is_a_vector) == 150)
--- recodefy/3/dataframe.R
+++ recodefy/3/dataframe.R
@@ -1,31 +1,40 @@
-#Ex 62f217738b1e5a10a4ba2a9e
-#Do not use additional R packages when solving this exercise.
-#a)
-#assume that titanic.csv is in the current working directory
-#load the titanic dataset into a variable called titanic
-#do not put setwd() in your codes
+# Ex 62f217738b1e5a10a4ba2a9e
+# Do not use additional R packages when solving this exercise.
+# a)
+# assume that titanic.csv is in the current working directory
+# load the titanic dataset into a variable called titanic
+# do not put setwd() in your codes
 
-#Complete implementation...
-titanic <-
-
-
-#b)
-#obtain the number of records in the dataset 
-numRecords <-
+# Complete implementation...
+titanic <- read.csv("titanic.csv")
 
 
-#c)
-#filter the dataset (using the dataframe indexing operator) to obtain 
-#a dataframe with only female passengers
-femalePassengers <-
+# b)
+# obtain the number of records in the dataset
+numRecords <- nrow(titanic)
 
 
-#d)
-#what is the ratio of female passenger who survived over the total number
-#of female passengers?
-question_d <- function(){
-  #Complete implementation...
-  ratio <-
+# c)
+# filter the dataset (using the dataframe indexing operator) to obtain
+# a dataframe with only female passengers
+femalePassengers <- titanic[titanic[5] == "female", ]
 
-  return (ratio)
+
+
+# d)
+# what is the ratio of female passenger who survived over the total number
+# of female passengers?
+question_d <- function() {
+
+
+  # Complete implementation...
+
+  female_total <- nrow(femalePassengers)
+  survived_flag <- 1
+  female_survived <- nrow(femalePassengers[femalePassengers[2] == survived_flag, ])
+  ratio <- female_survived / female_total
+  print(ratio)
+  return(ratio)
 }
+
+question_d()
