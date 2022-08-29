d466d9057f9057950f449fe4b0fbeaceae953853
Noel Lim <noellimx@gmail.com>
2022-08-29 17:30:11 +0800

    format document

---
+++ T1/submission.R
@@ -0,0 +1,6 @@
+
+
+
+gplay <- read.csv("googleplaystore.csv")
+
+gplay_without_any_na <- na.omit(gplay)
--- recodefy/4/submission.R
+++ recodefy/4/submission.R
@@ -1,31 +1,27 @@
-#Ex 62f217738b1e5a10a4ba2aa3
-#a)
-#assume that AutoInsurSweden.txt is in the current working directory
-#load the AutoInsurSweden dataset into a variable called data using 
-#read.table()
-#make sure 2 headers (X, Y) are read in properly as the column names
+# Ex 62f217738b1e5a10a4ba2aa3
+# a)
+# assume that AutoInsurSweden.txt is in the current working directory
+# load the AutoInsurSweden dataset into a variable called data using
+# read.table()
+# make sure 2 headers (X, Y) are read in properly as the column names
 
-#Complete implementation...
-data <- read.table("AutoInsurSweden.txt",skip=10, header=TRUE)
+# Complete implementation...
+data <- read.table("AutoInsurSweden.txt", skip = 10, header = TRUE)
 
 
-#b)
-#European countries often use comma to separate the integer
-#from the decimal part
-#read up on gsub() and try convert the Y column of data to 
-#the proper numeric form
+# b)
+# European countries often use comma to separate the integer
+# from the decimal part
+# read up on gsub() and try convert the Y column of data to
+# the proper numeric form
 
 print(data)
 print(class(data$Y)) # character
-data$Y <- as.numeric(gsub(",",".", data$Y))
+data$Y <- as.numeric(gsub(",", ".", data$Y))
 print(data)
 print(class(data$Y))
 
 
-model <- lm(X~Y,data)
+model <- lm(X ~ Y, data)
 
 print(summary(model))
-
-
-
-
